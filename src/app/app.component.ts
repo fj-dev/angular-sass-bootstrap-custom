1d87f28cafbf436a8e895471c2b44da12502ab38
fj-dev <foglemanj@gmail.com>
2020-05-15 14:55:37 -0400

    8 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  globalTheme = 'dark';
+  navbarColor = 'dark';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faEllipsisV,
+  faUser,
+  faDragon,
+  faMapMarker,
+  faStar,
+  faMoon as faSolidMoon
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faMoon
+} from '@fortawesome/free-regular-svg-icons';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MyDialogsModule } from './dialogs/dialogs.module';
 import { MyButtonsModule } from './buttons/buttons.module';
 import { MyNavsModule } from './navs/navs.module';
@@ -16,15 +28,27 @@ import { MyNavbarComponent } from './my-navbar/my-navbar.component';
     BrowserModule, 
     FormsModule, 
     NgbModule,
+    FontAwesomeModule,
     MyButtonsModule,
     MyDialogsModule,
     MyNavsModule
   ],
   declarations:   [
     AppComponent,
-    HelloComponent,
     MyNavbarComponent
   ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faEllipsisV,
+      faUser,
+      faDragon,
+      faMoon,
+      faSolidMoon,
+      faMapMarker,
+      faStar
+    );
+  }
+}
