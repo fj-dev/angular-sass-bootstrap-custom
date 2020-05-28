2cc55ab7d0172d8a8378ef1bfca9075ade2f96d9
fj-dev <foglemanj@gmail.com>
2020-05-28 13:58:23 -0400

    1 file modified, 3 added

---
+++ src/app/forms/date-time-picker/date-time-picker2/date-time-picker2.component.ts
@@ -0,0 +1,143 @@
+import { Component, OnInit, Input, forwardRef, ViewChild } from '@angular/core';
+import { NgbTimeStruct, NgbDateStruct, NgbPopoverConfig, NgbPopover, NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { DateTimeModel } from '../date-time.model';
+
+@Component({
+    selector: 'date-time-picker',
+    templateUrl: './date-time-picker.component.html',
+    styleUrls: ['./date-time-picker.component.scss'],
+    providers: [
+        DatePipe,
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DateTimePickerComponent),
+            multi: true
+        }
+    ]
+})
+export class DateTimePickerComponent implements ControlValueAccessor, OnInit {
+    @Input()
+    dateString: string;
+
+    @Input()
+    inputDatetimeFormat = 'M/d/yyyy H:mm';
+    
+    private hourOptions = new Array(24).fill(0).map((x, i) => {return i;});
+    private minutesOptions = [0,30];
+    private defaultTimeOptions = new Array(48).fill({hour: 0, minute: 0, second: 0}).map((obj, idx) => {
+      let newObj = {hour: 0, minute: 0, second: 0};
+      
+      return newObj;
+    });
+
+    private datetime: DateTimeModel = new DateTimeModel();
+    private firstTimeAssign = true;
+
+    @ViewChild(NgbDatepicker, {static: true})
+    private dp: NgbDatepicker;
+
+    @ViewChild(NgbPopover, {static: true})
+    private popover: NgbPopover;
+
+    timeOptions = new Array[48];
+
+    onTouch: () => { };
+    onChange: (_: any) => { };
+
+    constructor(private config: NgbPopoverConfig) {
+        config.autoClose = 'outside';
+        config.placement = 'auto';
+    }
+
+    ngOnInit(): void {
+    }
+
+    writeValue(newModel: string) {
+        if (newModel) {
+            this.datetime = Object.assign(this.datetime, DateTimeModel.fromLocalString(newModel));
+            this.dateString = newModel;
+            this.setDateStringModel();
+        } else {
+            this.datetime = new DateTimeModel();
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouch = fn;
+    }
+
+    onInputChange($event: any) {
+        const value = $event.target.value;
+        const dt = DateTimeModel.fromLocalString(value);
+
+        if (dt) {
+            this.datetime = dt;
+            this.setDateStringModel();
+        } else if (value.trim() === '') {
+            this.datetime = new DateTimeModel();
+            this.dateString = '';
+            this.onChange(this.dateString);
+        } else {
+              this.onChange(value);
+        }
+    }
+
+    onDateChange($event) {        
+        if ($event.year){
+          $event = `${$event.year}-${$event.month}-${$event.day}`
+        }
+
+        const date = DateTimeModel.fromLocalString($event);
+   
+        if (!date) {
+            this.dateString = this.dateString;
+            return;
+        }
+
+        if (!this.datetime) {
+            this.datetime = date;
+        }
+
+        this.datetime.year = date.year;
+        this.datetime.month = date.month;
+        this.datetime.day = date.day;
+
+        if (this.dp) {
+          this.dp.navigateTo({ year: this.datetime.year, month: this.datetime.month });
+          console.log('navigated');
+        }
+        console.log('test');
+        this.setDateStringModel();
+    }
+
+    onTimeChange(event: NgbTimeStruct) {
+        this.datetime.hour = event.hour;
+        this.datetime.minute = event.minute;
+        this.datetime.second = event.second;
+
+        this.setDateStringModel();
+    }
+
+    setDateStringModel() {
+        this.dateString = this.datetime.toString();
+
+        if (!this.firstTimeAssign) {
+            this.onChange(this.dateString);
+        } else {
+            // Skip very first assignment to null done by Angular
+            if (this.dateString !== null) {
+                this.firstTimeAssign = false;
+            }
+        }
+    }
+
+    inputBlur($event) {
+        //this.onTouch();
+    }
+}
