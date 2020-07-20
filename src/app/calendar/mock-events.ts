9a4c3c700dd104c8efc1b16f22378bb589840ae7
fj-dev <foglemanj@gmail.com>
2020-07-20 13:45:17 -0400

    3 files modified, 1 deleted

--- src/app/calendar/mock-events.ts
+++ src/app/calendar/mock-events.ts
@@ -10,22 +10,32 @@ export class MockEvents {
         title: '1-hour Event',
         start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate(), 9, 30),
         end: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate(), 10, 30),
+        className: 'reg-event'
+      },
+      {
+        title: 'Long Event Title',
+        start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate(), 12, 30),
+        end: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate(), 13, 0),
+        className: 'long-title'
       },
       {
         title: 'Multi-Day Event',
-        start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()-9, 7, 0),
+        start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()-11, 7, 0),
         end: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()-7, 16, 30),
+        className: 'multi-day'
       },
       {
         title: 'All Day Event',
         start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()+3, 0, 0),
-        allDay: true
+        allDay: true,
+        className: 'all-day'
       },
       {
         title: 'Multi-Day All Day Event',
         start: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()+7, 0, 0),
         end: new Date(this.dt.getFullYear(), this.dt.getMonth(), this.dt.getDate()+9, 0, 0),
-        allDay: true
+        allDay: true,
+        className: 'multi-day all-day'
       },
     ];
   }
