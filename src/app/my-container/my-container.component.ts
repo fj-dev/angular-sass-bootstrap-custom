b4ac10029a1ea70296e606ef04a3c75f283c1075
fj-dev <foglemanj@gmail.com>
2020-09-17 10:53:10 -0400

    3 files modified

--- src/app/my-container/my-container.component.ts
+++ src/app/my-container/my-container.component.ts
@@ -8,7 +8,7 @@ import { ICardBase, CardBase, ICardContentBase, CardBodyContent } from './card-b
   styleUrls: ['./my-container.component.scss']
 })
 export class MyContainerComponent implements OnInit {
-  kitchenSinkCardContents = [
+  private kitchenSinkCardContents = [
     new CardBase({
       type: 'header', 
       bgColor: '', 
@@ -80,18 +80,60 @@ export class MyContainerComponent implements OnInit {
   private darkCard = this.getSimpleContent('dark');
   private lightCard = this.getSimpleContent('light');
   
+  private primaryCardHeader = this.getThemedHeaderSimpleContent('primary');
+  private secondaryCardHeader = this.getThemedHeaderSimpleContent('secondary');
+  private successCardHeader = this.getThemedHeaderSimpleContent('success');
+  private infoCardHeader = this.getThemedHeaderSimpleContent('info');
+  private warningCardHeader = this.getThemedHeaderSimpleContent('warning');
+  private dangerCardHeader = this.getThemedHeaderSimpleContent('danger');
+  private darkCardHeader = this.getThemedHeaderSimpleContent('dark');
+  private lightCardHeader = this.getThemedHeaderSimpleContent('light');
+  
   constructor() { }
 
   ngOnInit() {
   }
 
-  getSimpleContent(theme) {
+  private getSimpleContent(theme) {
     const simpleCardContents = [
       new CardBase({
         type: 'header', 
         bgColor: '', 
         borderColor: '', 
         textColor: ''
+      }),
+      new CardBase({
+        type: 'body', 
+        bgColor: '', 
+        borderColor: '', 
+        textColor: '', 
+        contents: [
+          new CardBodyContent({
+            contentType: 'title',
+            bgColor: '',
+            borderColor: '',
+            textColor: '',
+            displayText: theme.charAt(0).toUpperCase() + theme.slice(1) + ' card title'
+          }),
+          new CardBodyContent({
+            contentType: 'text',
+            bgColor: '',
+            borderColor: '',
+            textColor: ''
+          })
+        ]
+      })
+    ];
+    return simpleCardContents;
+  }
+
+  private getThemedHeaderSimpleContent(theme) {
+    const simpleCardContents = [
+      new CardBase({
+        type: 'header', 
+        bgColor: theme, 
+        borderColor: '', 
+        textColor: theme === 'light' ? 'dark' : 'light'
       }),
       new CardBase({
         type: 'body', 
