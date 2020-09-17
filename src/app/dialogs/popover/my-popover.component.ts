518f57ab7552f14c86f394dbe1c4e4fc0b18aba2
fj-dev <foglemanj@gmail.com>
2020-09-17 16:53:27 -0400

    8 files modified

--- src/app/dialogs/popover/my-popover.component.ts
+++ src/app/dialogs/popover/my-popover.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'my-popover',
@@ -6,5 +6,37 @@ import {Component} from '@angular/core';
   styleUrls: ['./my-popover.component.scss']
 })
 export class MyPopoverComponent {
+  private _placement = 'top';
+  @Input() set popoverPlacement(val) {
+    this._placement = val;
+  }
+  get popoverPlacement() {
+    return this._placement;
+  }
+
+  private _theme = 'my-custom-popover';
+  @Input() set theme(val) {
+    this._theme = 'my-custom-popover-' + val;
+  }
+  get theme() {
+    return this._theme;
+  }
+
+  private _buttonText = 'Popover';
+  @Input() set buttonText(val) {
+    this._buttonText = val;
+  }
+  get buttonText() {
+    return this._buttonText;
+  }
+
+  private _title = 'Popover Title';
+  @Input() set title(val) {
+    this._title = val;
+  }
+  get title() {
+    return this._title;
+  }
+
   constructor(){}
 }
