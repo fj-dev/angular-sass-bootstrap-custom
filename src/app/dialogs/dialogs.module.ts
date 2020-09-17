d8d6598773c835be07e5c20305a8f98f6a3956e4
fj-dev <foglemanj@gmail.com>
2020-09-17 12:52:05 -0400

    5 files modified

--- src/app/dialogs/dialogs.module.ts
+++ src/app/dialogs/dialogs.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { MyModalComponent } from './modal/my-modal.component';
 import { MyPopoverComponent } from './popover/my-popover.component';
 import { MyTooltipComponent } from './tooltip/my-tooltip.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [
     MyModalComponent,
--- src/app/dialogs/tooltip/my-tooltip.component.ts
+++ src/app/dialogs/tooltip/my-tooltip.component.ts
@@ -1,10 +1,42 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'my-tooltip',
   templateUrl: './my-tooltip.component.html',
-  styleUrls: ['./my-tooltip.component.scss']
+  styleUrls: ['./my-tooltip.component.scss'],
 })
 export class MyTooltipComponent {
-  constructor(){}
+  private _placement = 'top';
+  @Input() set tooltipPlacement(val) {
+    this._placement = val;
+  }
+  get tooltipPlacement() {
+    return this._placement;
+  }
+
+  private _theme = 'my-custom-tooltip';
+  @Input() set theme(val) {
+    this._theme = 'my-custom-tooltip-' + val;
+  }
+  get theme() {
+    return this._theme;
+  }
+  
+  private _buttonText = 'Tooltip on top';
+  @Input() set buttonText(val) {
+    this._buttonText = val;
+  }
+  get buttonText() {
+    return this._buttonText;
+  }
+
+  private _tooltip = 'Tooltip default ';
+  @Input() set tooltipContent(val) {
+    this._tooltip = val;
+  }
+  get tooltipContent() {
+    return this._tooltip;
+  }
+
+  constructor(){ }
 }
